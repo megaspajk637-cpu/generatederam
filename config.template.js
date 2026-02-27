1e932ec87f17dbeebc76a2448a2cc9b4e3dfc402
megaspajk637-cpu <megaspajk637@gmail.com>
2026-02-27 11:43:59 +0500

    Add files via upload

---
+++ config.template.js
@@ -0,0 +1,4 @@
+// config.template.js - скопируйте в config.js и вставьте свой ключ
+const CONFIG = {
+    API_KEY: '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' // Замените на свой ключ
+};
