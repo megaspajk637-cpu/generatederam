7b5f6c09e43d91febe79743b7da9f408a2434427
megaspajk637-cpu <megaspajk637@gmail.com>
2026-02-27 11:48:54 +0500

    Update config.template.js

--- config.template.js
+++ config.template.js
@@ -1,4 +1,5 @@
 // config.template.js - скопируйте в config.js и вставьте свой ключ
 const CONFIG = {
-    API_KEY: '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' // Замените на свой ключ
+    API_KEY: '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' // Замените на свой ключ
+
 };
